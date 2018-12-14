639f2d9aca2534d04d87b1d28ed5561cbd8aaf44
soohoonlee <jjinimania@gmail.com>
2018-12-14 11:11:25 +0900

    BaseControllerTest

--- src/test/java/me/ssoon/demoinflearnrestapi/events/EventControllerTest.java
+++ src/test/java/me/ssoon/demoinflearnrestapi/events/EventControllerTest.java
@@ -24,43 +24,18 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.time.LocalDateTime;
 import java.util.stream.IntStream;
-import me.ssoon.demoinflearnrestapi.common.RestDocsConfiguration;
+import me.ssoon.demoinflearnrestapi.common.BaseControllerTest;
 import me.ssoon.demoinflearnrestapi.common.TestDescription;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
-@AutoConfigureRestDocs
-@Import(RestDocsConfiguration.class)
-@ActiveProfiles("test")
-public class EventControllerTest {
-
-  @Autowired
-  private MockMvc mockMvc;
-
-  @Autowired
-  private ObjectMapper objectMapper;
+public class EventControllerTest extends BaseControllerTest {
 
   @Autowired
   private EventRepository eventRepository;
-
-  @Autowired
-  private ModelMapper modelMapper;
 
   @Test
   @TestDescription("정상적으로 이벤트를 생성하는 테스트")
